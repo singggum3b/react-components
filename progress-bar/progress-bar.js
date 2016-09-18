b9aa5bf38069111ff06aa2a0212c41e97ed96df5
Nghia Mai <mvtnghia@gmail.com>
2016-09-18 17:51:12 +0700

    Migrate/progressbar (#3)
    * - Migrate backtop
    
    * - Migrate backtop
    
    * - Migrate ProgressBar
    
    * - Migrate ProgressBar
    
    * - Migrate ProgressBar

---
+++ progress-bar/progress-bar.js
@@ -0,0 +1,34 @@
+import classNames from "classnames";
+import { propTypes } from "tcomb-react";
+import "./progress-bar.styl";
+
+export type ProgressBarPropsType = {
+	className?: string,
+	percent: number,
+	transitionDuration?: number, // milliseconds
+}
+
+export default function ProgressBar(props) {
+	let className = classNames({
+		"g-progress-bar": true,
+		[props.className]: props.className
+	});
+	let transitionDuration = props.transitionDuration ? props.transitionDuration / 1000 + "s" : 0 + "s";
+	let style = {
+		"width": ((props.percent >= 100) ? 100 : props.percent) + "%",
+		"transitionDuration": transitionDuration,
+		"WebkitTransitionDuration": transitionDuration,
+	};
+	return (
+		<div className={className}>
+			<div className="bar" style={style}></div>
+		</div>
+	);
+}
+
+ProgressBar.propsType = propTypes(ProgressBarPropsType, { strict: false });
+ProgressBar.displayName = "ProgressBar";
+ProgressBar.defaultProps = {
+	percent: "1",
+	transitionDuration: "2000",
+};
